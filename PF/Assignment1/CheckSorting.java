8bb84b2129f3892e2f7f2700067c6e4451bb0b4e
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-10 17:19:09 +0530

    Update CheckSorting.java

--- PF/Assignment1/CheckSorting.java
+++ PF/Assignment1/CheckSorting.java
@@ -1,61 +1,54 @@
+public class CheckSorting
+{
 
-public class CheckSorting {
+	public int checkSorting(int a[])
+	{
+		if (a[0] < a[1]) {
 
+			for (int i = 1; i < a.length - 1; i++) {
+				if (a[i] > a[i + 1]) {
 
-     public int checkSorting(int a[])
-     {
-    	if(a[0]<a[1])
-    	{
-    		
-    		for(int i=1;i<a.length-1;i++)
-    		{
-    			if(a[i]>a[i+1])
-    			{
-    				
-    				return 0;
-    				
-    			}else
-    			{
-    				continue;
-    			}
-    			
-    			
-    			
-    		}
-    		
-    		return 1;
-    		
-    	}else if(a[0]>a[1])
-    	{
-    		
-    		for(int i=1;i<a.length-1;i++)
-    		{
-    			if(a[i]<a[i+1])
-    			{
-    				
-    				return 0;
-    				
-    			}else
-    			{
-    				continue;
-    			}
-    			
-    			
-    			
-    		}
-    		
-    		return 2;
-    		
-    		
-    		
-    		
-    	}
-    	 
-    	 
-    	return 0; 
-    	 
-     }
+					return 0;
 
+				} else {
+					continue;
+				}
 
+			}
+
+			return 1;
+
+		} else if (a[0] > a[1]) {
+
+			for (int i = 1; i < a.length - 1; i++) {
+				if (a[i] < a[i + 1]) {
+
+					return 0;
+
+				} else {
+					continue;
+				}
+
+			}
+
+			return 2;
+
+		}
+
+		return 0;
+
+	}
+
+	public String returnMessage(int n)
+	{
+		String message = "";
+		if (n == 1)
+			message = "Array is in accending order";
+		else if (n == 2)
+			message = "Array is in decending order";
+		else
+			message = "Array is not sorted";
+		return message;
+	}
 
 }
