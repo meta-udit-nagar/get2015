a6044b6149a2b54eea4fde4011667a9a2706190d
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-10 17:20:53 +0530

    Update CheckSortingTest.java

--- PF/Assignment1/CheckSortingTest.java
+++ PF/Assignment1/CheckSortingTest.java
@@ -28,8 +28,33 @@ public class CheckSortingTest {
 	@Test
 	public void test() {
 		CheckSorting checkSorting= new CheckSorting();
-		int a[]={1,1,2};
-		assertEquals(0, checkSorting.checkSorting(a));
+		int a[]={1,2,3};
+		assertEquals(1, checkSorting.checkSorting(a));
+		
+		
+		int a1[]={1,2,1};
+		assertEquals(0, checkSorting.checkSorting(a1));
+		
+		
+		int a2[]={3,2,1};
+		assertEquals(2, checkSorting.checkSorting(a2));
 	}
 
+	
+	
+	
+	@Test
+	public void messageTest() {
+		CheckSorting checkSorting= new CheckSorting();
+		int a=1;
+		assertEquals("Array is in accending order", checkSorting.returnMessage(a));
+		
+		
+		int a1=2;
+		assertEquals("Array is in decending order", checkSorting.returnMessage(a1));
+		
+		
+		int a2=0;
+		assertEquals("Array is not sorted", checkSorting.returnMessage(a2));
+	}
 }
