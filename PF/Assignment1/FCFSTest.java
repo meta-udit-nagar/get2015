26c7bdc2a9b88dee5b96a7f3333f12b5f2909681
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-10 17:25:23 +0530

    Update FCFSTest.java

--- PF/Assignment1/FCFSTest.java
+++ PF/Assignment1/FCFSTest.java
@@ -6,15 +6,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
-public class FCFSTest 
-{
-	int [][]expected = new int[][] { { 1,1,0,1,12},
-			{2,5,8,13,19}, {3,9,11,20,21}, {4,25,0,25,29}
-	};
-	int []arrival_time = new int[] {1, 5, 9, 25};
-	int []job_size = new int[] {12, 7, 2, 5};
-
+public class FCFSTest {
+	int[][] expected = new int[][] { { 1, 1, 0, 1, 12 }, { 2, 5, 8, 13, 19 },
+			{ 3, 9, 11, 20, 21 }, { 4, 25, 0, 25, 29 } };
+	int[] arrival_time = new int[] { 1, 5, 9, 25 };
+	int[] job_size = new int[] { 12, 7, 2, 5 };
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -32,26 +28,26 @@ public class FCFSTest
 	public void tearDown() throws Exception {
 	}
 
-	public int isArrayEqual(int expected[][],int output[][])
-	{
-		int loopCount1,loopCount2,error=0;
-		for(loopCount1=0; loopCount1<4;loopCount1++)
-		{
-			for(loopCount2=0; loopCount2<5;loopCount2++)
-			{
-				if(expected[loopCount1][loopCount2]!=output[loopCount1][loopCount2])
-					error=1;
+	public static boolean isArrayEqual(int expected[][], int output[][]) {
+		boolean b = true;
+		int loopCount1;
+		int loopCount2;
+		for (loopCount1 = 0; loopCount1 < 4; loopCount1++) {
+			for (loopCount2 = 0; loopCount2 < 5; loopCount2++) {
+				if (expected[loopCount1][loopCount2] != output[loopCount1][loopCount2])
+					return false;
 			}
 		}
-		return error;
+		return b;
 	}
-	
-	
+
 	@Test
-	public void test()
-	{
-		FCFS obj = new FCFS();
-		assertEquals(0, isArrayEqual(expected,obj.FCFS(arrival_time ,job_size  , 4)));
+	public void test() {
+
+		assertEquals(
+				true,
+				FCFSTest.isArrayEqual(expected,
+						FCFS.fcfs(arrival_time, job_size)));
 	}
 
 }
