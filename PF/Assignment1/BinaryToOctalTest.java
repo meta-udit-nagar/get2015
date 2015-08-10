cd8580c04da3915ddd3109e935b26b5f547c31f7
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-10 17:18:23 +0530

    Update BinaryToOctalTest.java

--- PF/Assignment1/BinaryToOctalTest.java
+++ PF/Assignment1/BinaryToOctalTest.java
@@ -25,11 +25,59 @@ public class BinaryToOctalTest {
 	public void tearDown() throws Exception {
 	}
 
+	
+
+	
 	@Test
+	
 	public void binaryToOctalTest() {
-		BinaryToOctal binaryToOctal= new BinaryToOctal();
+		int n=1011;
+		int nCopy=n;
+		int lastDigit;
+			while(nCopy!=0)
+			{
+				lastDigit=nCopy%10;
+				if(lastDigit>1)
+				{
+					
+					fail("error");
+				}
+				nCopy=nCopy/10;
+				
+			}
+			System.out.print(""+BinaryToOctal.convertBinaryToOctal(n));
+		assertEquals(13, BinaryToOctal.convertBinaryToOctal(n));
+				
+		 n=0000;
+		 nCopy=n;
 		
-		assertEquals(13,binaryToOctal.binaryToOctal(1011) );
+			while(nCopy!=0)
+			{
+				lastDigit=nCopy%10;
+				if(lastDigit>1)
+				{
+					
+					fail("error");
+				}
+				nCopy=nCopy/10;
+				
+			}
+		assertEquals(0, BinaryToOctal.convertBinaryToOctal(n));
+	      n=111;
+		  nCopy=n;
+	
+			while(nCopy!=0)
+			{
+				lastDigit=nCopy%10;
+				if(lastDigit>1)
+				{
+					
+					fail("error");
+				}
+				nCopy=nCopy/10;
+				
+			}
+		assertEquals(7, BinaryToOctal.convertBinaryToOctal(n));
 		
 	}
 
