2223622f9f8b031d2ea632b64985c96e693cb2f8
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-11 16:18:36 +0530

    Update ReverseRightTringleTest.java

--- PF/Assignment2/ReverseRightTringleTest.java
+++ PF/Assignment2/ReverseRightTringleTest.java
@@ -27,9 +27,11 @@ public class ReverseRightTringleTest {
 	@Test
 	public void testSpaces() {
 
-		assertEquals(" ", ReverseRightTringle.spaces(2, 5));
-		assertEquals("", ReverseRightTringle.spaces(1, 5));
-		assertEquals("    ", ReverseRightTringle.spaces(5, 5));
+		assertEquals(1, ReverseRightTringle.spaces(2, 5).length());
+		assertEquals(0, ReverseRightTringle.spaces(1, 5).length());
+		assertEquals(4, ReverseRightTringle.spaces(5, 5).length());
+		assertEquals("wrong input", ReverseRightTringle.spaces(100, 5));
+		assertEquals("wrong input", ReverseRightTringle.spaces(-5, 5));
 
 	}
 
@@ -38,6 +40,8 @@ public class ReverseRightTringleTest {
 		assertEquals("12345", ReverseRightTringle.numbers(1, 5));
 		assertEquals("1", ReverseRightTringle.numbers(5, 5));
 		assertEquals("1234", ReverseRightTringle.numbers(2, 5));
+		assertEquals("wrong input", ReverseRightTringle.numbers(100, 2));
+		assertEquals("wrong input", ReverseRightTringle.spaces(-5, 5));
 	}
 
 	@Test
