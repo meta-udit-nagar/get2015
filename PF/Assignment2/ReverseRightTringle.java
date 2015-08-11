9460c03e5047fffa6c1502a7732b716efa347d7f
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-11 16:17:27 +0530

    Update ReverseRightTringle.java

--- PF/Assignment2/ReverseRightTringle.java
+++ PF/Assignment2/ReverseRightTringle.java
@@ -1,24 +1,29 @@
 public class ReverseRightTringle {
 	public static String spaces(int row, int n) {
-		String outputSpaces = "";
+		if (row < 2 * n && row > 0) {
+			String outputSpaces = "";
 
-		for (int count = 0; count < row - 1; count++) {
+			for (int count = 0; count < row - 1; count++) {
 
-			outputSpaces = outputSpaces + " ";
-		}
+				outputSpaces = outputSpaces + " ";
+			}
 
-		return outputSpaces;
+			return outputSpaces;
+		} else
+			return "wrong input";
 	}
 
 	public static String numbers(int rows, int n) {
 		String outputNumber = "";
+		if (rows < 2 * n && rows > 0) {
+			for (int count = 1; count <= (n - rows + 1); count++) {
 
-		for (int count = 1; count <= (n - rows + 1); count++) {
+				outputNumber = outputNumber + count;
 
-			outputNumber = outputNumber + count;
-
-		}
-		return outputNumber;
+			}
+			return outputNumber;
+		} else
+			return "wrong input";
 	}
 
 	public static String[] reversrRightTringle(int n) {
@@ -26,7 +31,7 @@ public class ReverseRightTringle {
 		String outputTringle[] = new String[n];
 
 		for (int count = 0; count < n; count++) {
-			
+
 			outputTringle[count] = ReverseRightTringle.spaces(count + 1, n)
 					+ ReverseRightTringle.numbers(count + 1, n);
 		}
