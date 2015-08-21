fce1d4e3d3e6835bc0ccff4f9189f5c0f90c0cdd
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-22 01:08:17 +0530

    Update InputAction.java

--- OOP/InputAction.java
+++ OOP/InputAction.java
@@ -1,39 +1,46 @@
 import java.util.Scanner;
 
+public class InputAction extends Menu {
+	private static Train.Type type;
 
-public class InputAction implements Action{
-static private Train.Type type;
-	public void  perform(int input)
-	{
-		
-	if(input==1)
-	{
-		
-		type=Train.Type.PASSENGER;
-		
-		
-	}else
-	{
-		
-		type=Train.Type.GOODS;
-		
+	public InputAction() {
+
+		addMenuItem(new ActionableMenuItem("1-passenger", new PassengerAction()));
+		addMenuItem(new ActionableMenuItem("1-Goods", new GoodsAction()));
 	}
-		
+
+	class PassengerAction implements Action {
+		@Override
+		public void perform() {
+			// TODO Auto-generated method stub
+
+			type = Train.Type.PASSENGER;
+
+		}
 	}
-	
+
+	class GoodsAction implements Action {
+		@Override
+		public void perform() {
+			// TODO Auto-generated method stub
+			type = Train.Type.GOODS;
+		}
+	}
+
 	public static void InputProcess() {
 		while (true) {
 			Scanner scanner = new Scanner(System.in);
 			System.out
 					.println("welcome to Railways ticket booking center Enter your name");
 			String name = scanner.next();
-			MenuNew menuNew=new MenuNew();
-			
-			showChart(type);
+			Menu trainMenu = new InputAction();
+			trainMenu.display();
+
+			PrintOutput.showChart(type);
 			System.out.println("Enter from to stations");
 			String fromto = scanner.next();
 			System.out.println(fromto);
-			showChart(type, fromto);
+			PrintOutput.showChart(type, fromto);
 			System.out.println("Enter Train Name");
 			String trainName = scanner.next();
 			int noOfSeat = 0;
@@ -45,27 +52,12 @@ static private Train.Type type;
 
 			}
 			noOfSeat = scanner.nextInt();
-			if (noOfSeat <= getAvailable(trainName)) {
+			if (noOfSeat <= PrintOutput.getAvailable(trainName)) {
+
+				Menu paymentMenu = new PaymentMenu();
+				paymentMenu.display();
+
 				int amount = getFare(100, 50, type, noOfSeat);
-
-				System.out
-						.println("enter the mode of payment\n1-cc\n\2-wallet\n3-netbanking");
-				int in = scanner.nextInt();
-				Payment payment;
-				if (in == 1) {
-					 payment = new CcPayment();
-					
-
-				} else if (in == 2) {
-					 payment = new WalletPayment();
-					
-
-				} else {
-					 payment = new NetBankingPayment();					
-					
-
-				}
-				   payment.process(amount);
 
 				for (Train train : RailwayReservationSystem.trainList) {
 
@@ -75,7 +67,7 @@ static private Train.Type type;
 					}
 				}
 
-				printTicket(name, trainName, noOfSeat, amount);
+				PrintOutput.printTicket(name, trainName, noOfSeat, amount);
 
 			} else {
 
@@ -89,7 +81,7 @@ static private Train.Type type;
 				break;
 			} else {
 
-				showChart(type, fromto);
+				PrintOutput.showChart(type, fromto);
 			}
 
 		}
@@ -110,56 +102,4 @@ static private Train.Type type;
 		return amount;
 	}
 
-
-	private static void showChart(Train.Type type, String fromto) {
-		for (Train train : RailwayReservationSystem.trainList) {
-			if (train.type.equals(type) && train.getFromto().equals(fromto)) {
-
-				System.out.println(train.getName() + "    "
-						+ train.getDuration() + " hrs" + "    "
-						+ train.getAvail());
-
-			}
-
-		}
-
-	}
-
-	private static void showChart(Train.Type type) {
-
-		for (Train train : RailwayReservationSystem.trainList) {
-			if (train.type.equals(type)) {
-
-				System.out.println(train.getName() + "    "
-						+ train.getDuration() + " hrs" + "    "
-						+ train.getAvail() + "    " + train.getFromto());
-
-			}
-
-		}
-
-	}
-
-	private static void printTicket(String name, String trainName,
-			int noOfSeat, int amount) {
-
-		System.out.println("Name  : " + name);
-		System.out.println("train name  : " + trainName);
-		System.out.println("no of seat  : " + noOfSeat);
-		System.out.println("Amount  : " + amount);
-	}
-
-	private static int getAvailable(String trainName) {
-		for (Train train : RailwayReservationSystem.trainList) {
-			if (train.getName().equals(trainName))
-				return train.getAvail();
-
-		}
-
-		return -1;
-
-	}
-
-	
-	
 }
