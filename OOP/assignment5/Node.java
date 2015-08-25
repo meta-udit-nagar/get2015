4a412a3aa52fc1692c0bb66d16272aa294257fd6
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-25 18:02:12 +0530

    Update Node.java

--- OOP/assignment5/Node.java
+++ OOP/assignment5/Node.java
@@ -1,58 +1,31 @@
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+package socialNetworking;
 
 public class Node {
-	ArrayList<Entity> personList = new ArrayList<Entity>();
-	ArrayList<Entity> organisation = new ArrayList<Entity>();
 
-	public ArrayList<Entity> addNode() throws IOException {
+	private String name;
 
-		try {
-			StringTokenizer stringTokenizer;
-			String str = "D://Person/Person.txt";
-			FileInputStream fin = new FileInputStream(new File(str));
-			DataInputStream in = new DataInputStream(fin);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String personData = "";
-			int i = 1;
-			String email = "", name = "", phoneNumber = "", interest = "";
-			while ((personData = br.readLine()) != null) {
-				Person person;
-				stringTokenizer = new StringTokenizer(personData, ",");
-				i = 1;
-				while (stringTokenizer.hasMoreTokens()) {
-					if (i == 1) {
-						email = stringTokenizer.nextToken();
-					} else if (i == 2) {
-						name = stringTokenizer.nextToken();
-					} else if (i == 3) {
-						phoneNumber = stringTokenizer.nextToken();
-					} else {
-						interest = stringTokenizer.nextToken();
-					}
+	public String getName() {
+		return name;
+	}
 
-					i++;
-				}
-				long phoneNum = Long.parseLong(phoneNumber);
+	public void setName(String name) {
+		this.name = name;
+	}
 
-				person = new Person(phoneNum, email, name);
-				person.setInterest(interest);
-				personList.add(person);
+	@Override
+	public boolean equals(Object node) {
+		if (this.name.equals(((Node) node).getName()))
+			return true;
+		else
+			return false;
 
-			}
+	}
 
-			fin.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return personList;
+	@Override
+	public int hashCode() {
+		int hash = 3;
+		hash = 7 * hash + this.getName().hashCode();
+		return hash;
 	}
 
 }
