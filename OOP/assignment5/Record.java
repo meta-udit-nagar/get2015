16884e4a07ce563d5274185dc9071c35f0cb7dcb
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-25 18:04:31 +0530

    Update Record.java

--- OOP/assignment5/Record.java
+++ OOP/assignment5/Record.java
@@ -1,12 +1,7 @@
+package socialNetworking;
 
 public interface Record {
 
-	public void setInterest(String interest);
-
-	public String getInterest();
-
-	public void setPlacementRecord(String placementRecord);
-
-	public String getPlacementRecord();
+	public Record getRecord();
 
 }
